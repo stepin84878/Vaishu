2733f512a79f509230f4a87bed388c948757ba84
stepin84878 <69247364+stepin84878@users.noreply.github.com>
2020-09-18 23:39:24 +0530

    Add files via upload

---
+++ inc/calculate_operation.h
@@ -0,0 +1,16 @@
+#include "cal.h"
+#ifndef CAL_H
+#define CAL_H
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#include<stdlib.h>
+void addition();
+void subtraction();
+void multiplication();
+void division();
+void modulus();
+void power();
+int factorial();
+void calculator_operations();
+#endif
